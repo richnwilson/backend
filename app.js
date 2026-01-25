e2efb2ca3d9a32ea08966aa0129d147fe6473072
richnwilson <richnwilson@gmail.com>
2026-01-24 18:52:45 -0700

    show files

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import multer from 'multer';
+import { promises as fsPromises} from 'node:fs';
+
+import csv from 'csvtojson';
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import uploads from './models/uploads.js'
 
@@ -21,6 +30,9 @@ app.use(express.json());
 import cors from "cors";
 app.use(cors());
 
+// Middleware to parse raw body as text
+app.use(express.text({ type: 'text/csv' }));
+
 import dotenv from 'dotenv';
 dotenv.config({
     path: './.env'
@@ -38,6 +50,9 @@ mongoose
     })
 
 
+// Allow stored files to be accessible via browser
+app.use("/uploads", express.static("uploads"))    
+
 app.get("/", async (req,res) => {
     res.send("Success")
 })
@@ -49,8 +64,40 @@ app.post("/uploadFile", upload.single("file"), async (req, res) => {
         await uploads.create({title, filename})
         res.status(200).send("Upload successfully.")
     } catch(e) {
-        res.status(400).send(e?.message || 'Unknown error')
+        res.status(400).send(e?.message || 'Unknown error uploading files')
     }
+})
+
+app.get("/getFiles", async (req,res) => {
+    try {
+        const data = await uploads.find({}).lean().select({_id: 0, createdAt: 0, updatedAt: 0}).exec();
+        res.status(200).send({status: "ok",data})
+    } catch (e) {
+        res.status(400).send(e?.message || 'Unknown error trying to get files')
+    }
+})
+
+app.get("/getFile/:filename", async (req,res) => {
+try {
+    const { filename = ''} = req.params;
+    if (filename === "")  res.status(400).send("Missing file")
+
+    const filePath = `./uploads/${filename}`;
+    try {
+        await fsPromises.access(filePath, fsPromises.constants.F_OK)  
+    } catch(e) {
+        throw Error ("No such file")
+    }
+
+    csv()
+    .fromFile(filePath)
+    .then((jsonObj) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send({status: "ok", data: JSON.stringify(jsonObj)});
+    })
+} catch (e) {
+    res.status(400).send(e?.message || 'Unknown error trying to get file')
+}
 })
 
 // handle undefined Routes
